ff7e7222deafe702557c6dc277aa0722b8be39d0
lauk20 <klau20@stuy.edu>
2022-07-03 18:23:33 -0400

    Fix Navbar mobile responsiveness

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,6 +6,7 @@ import {
   IconButton,
   Box,
   MenuItem,
+  Grid,
 } from "@mui/material"
 import MenuIcon from '@mui/icons-material/Menu';
 import AddIcon from '@mui/icons-material/Add';
@@ -21,31 +22,40 @@ const NavBar = ({setOpenCourseDialog}) => {
     <>
     <AppBar color="primary">
       <Toolbar>
-        <IconButton sx={{color:"white", mr:1}} size="large">
-          <MenuIcon/>
-        </IconButton>
-        <Typography variant="h6" sx={{mr: 1, letterSpacing:"0.3rem"}}>
-          MYCOURSE
-        </Typography>
-        <Box>
-          <MenuItem sx={{borderRadius:"8px"}} component={ Link } to="/courses">
-            <Typography>
-                COURSES
+        <Grid container>
+          <Grid item>
+            <IconButton sx={{color:"white", mr:1}} size="large">
+              <MenuIcon/>
+            </IconButton>
+          </Grid>
+          <Grid item display="flex" justifyContent="flex-start" alignItems="center">
+            <Typography variant="h6" sx={{mr: 1, letterSpacing:"0.3rem"}}>
+              MYCOURSE
             </Typography>
-          </MenuItem>
-        </Box>
-        <Box>
-          <IconButton sx={{color:"white", mr:1}} size="large" onClick={openDialog}>
-            <AddIcon/>
-          </IconButton>
-        </Box>
-        <Box sx={{flexGrow:1}}/>
-        <Typography>NAME</Typography>
-        <Box>
-          <IconButton sx={{color:"white", mr:1}} size="large">
-            <AccountCircleIcon/>
-          </IconButton>
-        </Box>
+          </Grid>
+          <Grid item xs display="flex" justifyContent="flex-start" alignItems="center">
+            <Box>
+              <MenuItem sx={{borderRadius:"8px"}} component={ Link } to="/courses">
+                <Typography>
+                    COURSES
+                </Typography>
+              </MenuItem>
+            </Box>
+            <Box>
+              <IconButton sx={{color:"white", mr:1}} size="large" onClick={openDialog}>
+                <AddIcon/>
+              </IconButton>
+            </Box>
+          </Grid>
+          <Grid item display="flex" justifyContent="flex-start" alignItems="center">
+            <Typography>NAME</Typography>
+            <Box>
+              <IconButton sx={{color:"white", mr:1}} size="large">
+                <AccountCircleIcon/>
+              </IconButton>
+            </Box>
+          </Grid>
+        </Grid>
       </Toolbar>
     </AppBar>
     <Toolbar/>
