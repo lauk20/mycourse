26b67308a9c17dd39cf3bee1be14ca384d708a89
lauk20 <klau20@stuy.edu>
2022-07-10 13:11:04 -0400

    Add user token check and set

--- src/App.js
+++ src/App.js
@@ -5,10 +5,28 @@ import Courses from "./pages/Courses"
 import CoursePage from "./pages/CoursePage"
 import Login from "./pages/Login"
 import AddCourse from "./pages/AddCourse"
-import { useState } from "react"
+import Signup from "./pages/Signup"
+import { useState, useEffect } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { setLogin } from "./reducers/loginReducers"
 
 function App() {
   const [openCourseDialog, setOpenCourseDialog] = useState(false)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const currentsession = window.localStorage.getItem("mycoursetoken");
+    if (currentsession) {
+      const userToken = JSON.parse(currentsession)
+      dispatch(setLogin(userToken))
+    }
+  }, [])
+
+  const userToken = useSelector(state => {
+    const login = state.login
+
+    return login;
+  })
 
   return (
     <>
@@ -17,10 +35,11 @@ function App() {
         <Router>
           <Nav setOpenCourseDialog={setOpenCourseDialog}/>
           <Routes>
-            <Route path ="/courses/:id" element={<CoursePage/>}/>
+            <Route path ="/courses/:id" element={userToken == null ? <Login/> : <CoursePage/>}/>
             <Route path="/addcourse" element={<AddCourse/>}/>
-            <Route path="/courses" element={<Courses openCourseDialog={openCourseDialog} setOpenCourseDialog={setOpenCourseDialog}/>}/>
+            <Route path="/courses" element={userToken == null ? <Login/> : <Courses openCourseDialog={openCourseDialog} setOpenCourseDialog={setOpenCourseDialog}/>}/>
             <Route path="/login" element={<Login/>}/>
+            <Route path="/signup" element={<Signup/>}/>
           </Routes>
         </Router>
       </div>
