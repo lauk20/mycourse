d8c3694c624777b4652886cc5ed562faa21c29c8
lauk20 <klau20@stuy.edu>
2022-07-15 17:06:07 -0400

    Refactor AssignmentDetailsCard to own component

---
+++ src/components/AssignmentDetailsCard.js
@@ -0,0 +1,115 @@
+import {
+  Grid,
+  Card,
+  CardActionArea,
+  CardContent,
+  Box,
+  Typography,
+  Avatar,
+  TextField,
+  Button,
+  Grow,
+} from "@mui/material"
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import { useState } from "react"
+import { differenceInDays, format, isBefore, isToday, parseISO } from "date-fns"
+import { styled } from "@mui/material/styles"
+
+const dateDisplay = (date) => {
+  const today = parseISO(format(new Date(), "yyy-MM-dd"));
+  const duedate = parseISO(date)
+  //console.log("DUE: ", duedate)
+  const diff = differenceInDays(duedate, today)
+  console.log(date, "A", duedate, today)
+  if (isBefore(duedate, today) && !isToday(duedate, today)) {
+    return "Due " + format(duedate, "MM-dd-yy") + " (Late)"
+  }
+  if (diff === 0) {
+    return "Due Today"
+  } else if (diff === 1) {
+    return "Due Tomorrow"
+  } else if (diff < 7 && diff > 0) {
+    return "Due " + format(duedate, "EEEE")
+  } else {
+    console.log(duedate)
+    return "Due " + format(duedate, "MM-dd-yy")
+  }
+}
+
+const timeDisplay = (assignment) => {
+  return format(parseISO(assignment.due), "h:m a")
+}
+
+const WhiteBorderTextField = styled(TextField)`
+  & label.Mui-focused {
+    color: white;
+  }
+  & .MuiOutlinedInput-root {
+    &.Mui-focused fieldset {
+      border-color: white;
+    }
+    &:hover fieldset {
+      border-color: white;
+    }
+  }
+  & .MuiOutlinedInput-notchedOutline {
+    border-color: white;
+  }
+`;
+
+const AssignmentDetailsCard = ({ assign, counter }) => {
+  const [open, setOpen] = useState(false);
+  const openDetails = () => {
+    setOpen(!open)
+  }
+
+  return (
+    <Grow key={assign._id} in={true} timeout={1000 + counter * 500}>
+      <Grid item key={assign._id} sx={{width: "100%"}} display="flex" justifyContent="center">
+        <Card sx={{width: "100%", maxWidth: 1015, backgroundColor: "rgb(35, 35, 35)"}}>
+            <CardActionArea onClick={openDetails}>
+              <Box display="flex" sx={{height: 85}}>
+                <Grid container>
+                  <Grid item sx={{height: "100%"}} display="flex" justifyContent="center" alignItems="center">
+                    <Box display="flex" justifyContent="center" alignItems="center" sx={{ml: 1}}>
+                      <Avatar sx={{width: "100", height: "100", backgroundColor: "rgb(25, 25, 25)"}}>
+                        <AssignmentIcon variant="large" sx={{color: "white"}}/>
+                      </Avatar>
+                    </Box>
+                  </Grid>
+                  <Grid item sx={{height: "100%"}} display="flex" justifyContent="flex-start" alignItems="center" xs>
+                    <Box display="flex" justifyContent="flex-start" alignItems="center" sx={{width: "85%", ml: 1}} fullWidth>
+                      <Typography color="white" variant="subtitle" sx={{width: "100%", fontWeight:"bold"}}>{assign.content}</Typography>
+                    </Box>
+                  </Grid>
+                  <Grid item sx={{height: "100%"}} display="flex" justifyContent="flex-start" alignItems="center">
+                    <Box display="flex" justifyContent="flex-start" alignItems="center" sx={{height: "100%", marginLeft: "auto", mr: 1}} fullWidth>
+                      <Typography color="white" variant="caption" sx={{width: "120%", overflowWrap: "break-word"}}>{dateDisplay(assign.due) + " - " + timeDisplay(assign)}</Typography>
+                    </Box>
+                  </Grid>
+                </Grid>
+              </Box>
+          </CardActionArea>
+          {open &&
+          <Grow in={true} timeout={1000}>
+          <CardContent>
+            <Grid container display="flex" flexDirection="column" spacing={2}>
+              <Grid item>
+                <Typography color="white" variant="subtitle" sx={{fontWeight: "bold"}}>Details</Typography>
+                <WhiteBorderTextField fullWidth InputProps={{style: {color: "white"}}} sx={{input: {color: "white"}}} multiline/>
+              </Grid>
+              <Grid item display="flex">
+                <Button sx={{backgroundColor: "rgb(25, 25, 25)", color: "white", mr: 2}}>Save Details</Button>
+                <Button sx={{backgroundColor: "rgb(25, 25, 25)", color: "white", mr: 2}}>Complete Assignment</Button>
+              </Grid>
+            </Grid>
+          </CardContent>
+          </Grow>
+          }
+        </Card>
+      </Grid>
+    </Grow>
+  )
+}
+
+export default AssignmentDetailsCard
--- src/pages/CoursePage.js
+++ src/pages/CoursePage.js
@@ -1,90 +1,26 @@
 import {
   Grid,
   Card,
-  CardActionArea,
   CardMedia,
-  CardContent,
   Box,
   Typography,
-  Avatar,
   IconButton,
-  TextField,
-  Button,
-  Grow,
 } from "@mui/material"
-import AssignmentIcon from '@mui/icons-material/Assignment';
 import AddIcon from "@mui/icons-material/Add"
 import SettingsIcon from "@mui/icons-material/Settings"
 import NewAssignmentDialog from "../components/NewAssignmentDialog"
+import AssignmentDetailsCard from "../components/AssignmentDetailsCard"
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 import { useSelector, useDispatch } from "react-redux"
-import { differenceInDays, format, isBefore, isToday, parseISO } from "date-fns"
 import { initializeCourses } from "../reducers/courseReducers"
-import { styled } from "@mui/material/styles"
-
-const dateDisplay = (date) => {
-  const today = parseISO(format(new Date(), "yyy-MM-dd"));
-  const duedate = parseISO(date)
-  //console.log("DUE: ", duedate)
-  const diff = differenceInDays(duedate, today)
-  console.log(date, "A", duedate, today)
-  if (isBefore(duedate, today) && !isToday(duedate, today)) {
-    return "Due " + format(duedate, "MM-dd-yy") + " (Late)"
-  }
-  if (diff === 0) {
-    return "Due Today"
-  } else if (diff === 1) {
-    return "Due Tomorrow"
-  } else if (diff < 7 && diff > 0) {
-    return "Due " + format(duedate, "EEEE")
-  } else {
-    console.log(duedate)
-    return "Due " + format(duedate, "MM-dd-yy")
-  }
-}
-
-const timeDisplay = (assignment) => {
-  return format(parseISO(assignment.due), "h:m a")
-}
-
-const WhiteBorderTextField = styled(TextField)`
-  & label.Mui-focused {
-    color: white;
-  }
-  & .MuiOutlinedInput-root {
-    &.Mui-focused fieldset {
-      border-color: white;
-    }
-    &:hover fieldset {
-      border-color: white;
-    }
-  }
-  & .MuiOutlinedInput-notchedOutline {
-    border-color: white;
-  }
-`;
 
 const CoursePage = () => {
   let counter = 1;
   const [openNewAssignDialog, setOpenNewAssignDialog] = useState(false)
-  const [open, setOpen] = useState(false);
-  const [assignID, setAssignID] = useState(false);
 
   const openAssign = () => {
     setOpenNewAssignDialog(true)
-  }
-
-  const openDetails = (id) => {
-    return (event) => {
-      if (id === assignID){
-        setOpen(false)
-        setAssignID(undefined)
-      } else {
-        setOpen(true)
-        setAssignID(id)
-      }
-    }
   }
 
   const token = useSelector(state => {
@@ -142,51 +78,7 @@ const CoursePage = () => {
       {
         assignments.map((assign) => { counter = counter + 1;
           return (
-          <Grow key={assign._id} in={true} timeout={1000 + counter * 500}>
-            <Grid item key={assign._id} sx={{width: "100%"}} display="flex" justifyContent="center">
-              <Card sx={{width: "100%", maxWidth: 1015, backgroundColor: "rgb(35, 35, 35)"}}>
-                  <CardActionArea onClick={openDetails(assign._id)}>
-                    <Box display="flex" sx={{height: 85}}>
-                      <Grid container>
-                        <Grid item sx={{height: "100%"}} display="flex" justifyContent="center" alignItems="center">
-                          <Box display="flex" justifyContent="center" alignItems="center" sx={{ml: 1}}>
-                            <Avatar sx={{width: "100", height: "100", backgroundColor: "rgb(25, 25, 25)"}}>
-                              <AssignmentIcon variant="large" sx={{color: "white"}}/>
-                            </Avatar>
-                          </Box>
-                        </Grid>
-                        <Grid item sx={{height: "100%"}} display="flex" justifyContent="flex-start" alignItems="center" xs>
-                          <Box display="flex" justifyContent="flex-start" alignItems="center" sx={{width: "85%", ml: 1}} fullWidth>
-                            <Typography color="white" variant="subtitle" sx={{width: "100%", fontWeight:"bold"}}>{assign.content}</Typography>
-                          </Box>
-                        </Grid>
-                        <Grid item sx={{height: "100%"}} display="flex" justifyContent="flex-start" alignItems="center">
-                          <Box display="flex" justifyContent="flex-start" alignItems="center" sx={{height: "100%", marginLeft: "auto", mr: 1}} fullWidth>
-                            <Typography color="white" variant="caption" sx={{width: "120%", overflowWrap: "break-word"}}>{dateDisplay(assign.due) + " - " + timeDisplay(assign)}</Typography>
-                          </Box>
-                        </Grid>
-                      </Grid>
-                    </Box>
-                </CardActionArea>
-                {open && assignID === assign._id &&
-                <Grow in={true} timeout={1000}>
-                <CardContent>
-                  <Grid container display="flex" flexDirection="column" spacing={2}>
-                    <Grid item>
-                      <Typography color="white" variant="subtitle" sx={{fontWeight: "bold"}}>Details</Typography>
-                      <WhiteBorderTextField fullWidth InputProps={{style: {color: "white"}}} sx={{input: {color: "white"}}} multiline/>
-                    </Grid>
-                    <Grid item display="flex">
-                      <Button sx={{backgroundColor: "rgb(25, 25, 25)", color: "white", mr: 2}}>Save Details</Button>
-                      <Button sx={{backgroundColor: "rgb(25, 25, 25)", color: "white", mr: 2}}>Complete Assignment</Button>
-                    </Grid>
-                  </Grid>
-                </CardContent>
-                </Grow>
-                }
-              </Card>
-            </Grid>
-          </Grow>
+            <AssignmentDetailsCard assign={assign} counter={counter}/>
           )}
         )
       }
